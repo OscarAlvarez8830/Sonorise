ab67323df292a8712bcda70a6bf12690abd46fef
Oscar Alvarez <oscar.alvarez.8830@gmail.com>
2018-02-09 23:26:56 -0500

    Begin styling LandingPage

--- frontend/components/landing/landing_page.jsx
+++ frontend/components/landing/landing_page.jsx
@@ -3,13 +3,22 @@ import { Link } from 'react-router-dom';
 
 export default props => {
   return (
-    <main id="landing-page">
-      <nav>
-        <Link to="/sign-up">SIGN UP</Link>
-        <Link to="/login">LOGIN</Link>
-      </nav>
-      <figure>
-        Sonorise
+    <main className="LandingPage">
+      <section className="LandingPage__section">
+        <h1 className="LandingPage__h1">Sonorise</h1>
+        <nav className="LandingPage__nav">
+          <Link className="LandingPage__link LandingPage__link--green-and-white" to="/sign-up">SIGN UP</Link>
+
+          <aside className="LandingPage__aside">
+            <span className="LandingPage__span">--</span>
+              ALREADY HAVE AN ACCOUNT?
+            <span className="LandingPage__span">--</span>
+          </aside>
+
+          <Link className="LandingPage__link LandingPage__link--white-and-black" to="/login">LOGIN</Link>
+        </nav>
+      </section>
+      <figure className="LandingPage__figure">
         Beautiful music for a beautiful life.
       </figure>
     </main>
