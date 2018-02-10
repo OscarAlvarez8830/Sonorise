6819e3b6ea30327de89c0ce4eab85510285f415f
Oscar Alvarez <oscar.alvarez.8830@gmail.com>
2018-02-09 20:36:49 -0500

    Add auth controllers and routes

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,28 @@
+class Api::SessionsController < ApplicationController
+  before_action :ensure_current_user, only: :destroy
+  def create
+    @user = User.find_by_email_and_pass(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user
+      login
+      render 'api/users/show'
+    else
+      render json: ['Invalid email or password'], status: 401
+    end
+  end
+
+  def destroy
+    logout
+    render json: {}
+  end
+
+  private
+  def ensure_current_user
+    if logged_in?
+      render json: ['Unable to complete request'], status: 422
+    end
+  end
+end
