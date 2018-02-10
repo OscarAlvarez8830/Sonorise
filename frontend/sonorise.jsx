48b0f95b3d8a82acfd8a4cd803275d3bf3770157
Oscar Alvarez <oscar.alvarez.8830@gmail.com>
2018-02-09 21:10:34 -0500

    Serve up basic frontend

---
+++ frontend/sonorise.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
+
+import configureStore from './store/store';
+
+const Root = ({ store }) => {
+  return (
+    <Provider store={store}>
+      <HashRouter>
+        <div>
+          Hello!
+        </div>
+      </HashRouter>
+    </Provider>
+  );
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  let store;
+  if (window.currentUser) {
+    store = configureStore({
+      session: window.currentUser.id,
+      users: { [window.currentUser.id]: window.currentUser },
+    });
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  ReactDOM.render(<Root store={store} />, root);
+})
